d700c0bd87486b5541ceccfd88965049e6b179bd
teey-tech <thiagobatistadg@hotmail.com>
2022-02-11 16:41:15 -0300

    @teey-tech [Corrigindo erros]

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,46 +27,43 @@ let mapa = document.querySelector('#mapa')
 
 function validaNome() {
   let txt = document.querySelector('#txtNome')
-  if (nome.value.length <= 3 && nome.value.length != 0) {
+  if (nome.value.length <= 3) {
     txt.innerHTML = 'Nome Inválido'
-    txt.style.color = '#90ee90'
     txt.style.color = '#e71837'
-    txt.style.visibility = 'visible'
-    txt.style.marginTop = '0'
-  } else if (nome.value.length == 0) {
-    txt.style.visibility = 'hidden'
-    txt.style.marginTop = '-2rem'
   } else {
     txt.innerHTML = 'Nome Válido'
     txt.style.color = '#90ee90'
-    txt.style.visibility = 'visible'
-    txt.style.marginTop = '0'
     nomeOk = true
   }
   // console.log(nome.value)
 }
 
+// function validaEmail() {
+//   let txtEmail = document.querySelector('#txtEmail')
+//   if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+//     txtEmail.innerHTML = 'E-mail Inválido'
+//     txtEmail.style.color = '#90ee90'
+//     txtEmail.style.color = '#e71837'
+//     txtEmail.style.visibility = 'visible'
+//     txtEmail.style.marginTop = '0'
+//   } else {
+//     txtEmail.innerHTML = 'E-mail Válido'
+//     txtEmail.style.color = '#90ee90'
+//     txtEmail.style.visibility = 'visible'
+//     txtEmail.style.marginTop = '0'
+//     emailOk = true
+//   }
+// }
+
 function validaEmail() {
   let txtEmail = document.querySelector('#txtEmail')
-  if (
-    email.value.indexOf('@') == -1 &&
-    email.value.length <= 5 &&
-    email.value.length != 0
-  ) {
-    txtEmail.innerHTML = 'E-mail Inválido'
+  if (email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    txtEmail.innerHTML = 'E-mail Valido'
     txtEmail.style.color = '#90ee90'
-    txtEmail.style.color = '#e71837'
-    txtEmail.style.visibility = 'visible'
-    txtEmail.style.marginTop = '0'
-  } else if (email.value.length == 0) {
-    txtEmail.style.visibility = 'hidden'
-    txtEmail.style.marginTop = '-2rem'
-  } else {
-    txtEmail.innerHTML = 'E-mail Válido'
-    txtEmail.style.color = '#90ee90'
-    txtEmail.style.visibility = 'visible'
-    txtEmail.style.marginTop = '0'
     emailOk = true
+  } else {
+    txtEmail.innerHTML = 'E-mail Invalido'
+    txtEmail.style.color = '#e71837'
   }
 }
 
@@ -74,20 +71,12 @@ function validaAssunto() {
   let txtAssunto = document.querySelector('#txtAssunto')
   if (assunto.value.length >= 100 && assunto.value.length != 0) {
     txtAssunto.innerHTML = 'Assunto muito grande digite apenas 100 caracteres!'
-    txtAssunto.style.color = '#90ee90'
     txtAssunto.style.color = '#e71837'
-    txtAssunto.style.visibility = 'visible'
     txtAssunto.style.marginLeft = '6rem'
-    txtAssunto.style.marginTop = '0'
-  } else if (assunto.value.length == 0) {
-    txtAssunto.style.visibility = 'hidden'
-    txtAssunto.style.marginTop = '-1rem'
   } else {
     txtAssunto.innerHTML = 'Assunto Válido'
     txtAssunto.style.color = '#90ee90'
-    txtAssunto.style.visibility = 'visible'
     txtAssunto.style.marginLeft = '6rem'
-    txtAssunto.style.marginTop = '0'
     assuntoOk = true
   }
 }
